1e0beed9c975002817e014c44f297ed3c298e06c
Ignacio Alquati <ignacioalquati@MacBook-Pro.fibertel.com.ar>
2022-03-29 21:09:15 -0300

    Haciendo lío con Jorge

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("Este es el archivo index.js");
+console.log("Hola jorge, perdón por sobreescribir tu linea de código")
+// Seguro vos también reescribiste mi linea
+console.log("Espero que funcione bien después")
